ebbbe6e584fe76a593b40789866430b785a35974
nwigi <bnitelf@gmail.com>
2020-11-10 13:50:04 +0700

    Add Usage example

---
+++ ArcObjectX_Sample/ExampleLoopLayer/LayerLooperExample.cs
@@ -0,0 +1,25 @@
+﻿using ArcObjectX.DataProcessing;
+using ESRI.ArcGIS.Geodatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcObjectX_Sample.ExampleLoopLayer
+{
+    class LayerLooperExample
+    {
+        public void Run()
+        {
+            IFeatureClass fclass = null;
+            IQueryFilter filter = null;
+            bool readOnly = true;
+            var listResults = LayerLooper.Loop(fclass, filter, readOnly, (feature) =>
+            {
+                return new List<string>();
+                //return 1;
+            });
+        }
+    }
+}
