8fe627f269baef700125ea0313e26bed4d760af0
CHAWALWIT PATTANAKIWIWAT <Z00372@merkator.co.th>
2020-11-13 20:21:37 +0700

    Add Function Export and Example

--- ArcObjectX_Sample/ExampleExportTool/ExportToolExample.cs
+++ ArcObjectX_Sample/ExampleExportTool/ExportToolExample.cs
@@ -1,12 +1,42 @@
-﻿using ArcObjectX.DataManagement.Tool;
+﻿using System.Collections.Generic;
+
+using ESRI.ArcGIS.Geodatabase;
+
+using ArcObjectX.DataManagement.Tool;
 namespace ArcObjectX_Sample.ExampleExportTool
 {
     class ExportToolExample
     {
-        public void Run()
+        public void CaseExportAllInfo()
         {
-            ArcObjectExportTool exportTool = new ArcObjectExportTool();
-            
+            IFeatureClass fClass = null;
+
+            ArcObjectExportTool<object> arcObjectExportTool = new ArcObjectExportTool<object>();
+            arcObjectExportTool.ExportInfo(new List<object>())
+                .ExportAll(true)
+                .ExportTo(fClass)
+                .OutputType(OutputType.GDB)
+                .SaveEveryNRecord(100)
+                .Insert();
         }
+
+        public void CaseExportSpecifiedInfo()
+        {
+            IFeatureClass fClass = null;
+
+            ArcObjectExportTool<info> arcObjectExportTool = new ArcObjectExportTool<info>();
+            arcObjectExportTool.ExportInfo(new List<info>())
+                .ExportAll(false)
+                .ExportTo(fClass)
+                .OutputType(OutputType.GDB)
+                .SaveEveryNRecord(100)
+                .MappingField(x => x.Ref_OID, "Ref_OID")
+                .Insert();
+        }
+    }
+
+    public class info
+    {
+        public string Ref_OID { get; set; }
     }
 }
